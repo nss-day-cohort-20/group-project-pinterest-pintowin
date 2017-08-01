2c7ac9e9bab16a0d539e4564f4b4d9648501f6e5
Sam Staff <samstaff@Sams-MacBook-Pro.local>
2017-08-01 13:28:28 -0500

    current board object create and pin add code, still need pin obj before moving further

--- app/controllers/boardController.js
+++ app/controllers/boardController.js
@@ -2,3 +2,55 @@
 
 //will take data from board factory and format the recieved object to be diplayed in the view.
 //has button functionality for adding new boards
+
+app.controller("BoardController", function($scope, $window, BoardFactory, UserFactory) {
+
+    let currentUser = null;
+
+    UserFactory.isAuthenticated()
+        .then((user) => {
+            console.log("isAuthenticated called in board Controller logged user", user);
+            currentUser = UserFactory.getUser();
+            fetchBoards();
+        });
+
+    function fetchBoards() {
+        let boardArr = [];
+        console.log("fetchBoards called");
+        BoardFactory.getBoardList(currentUser)
+            .then((boardList) => {
+                console.log("board Data from fetchBoards", boardList);
+                let boardData = boardList.data;
+                Object.keys(boardData).forEach((key) => {
+                    boardData[key].id = key;
+                    boardArr.push(boardData[key]);
+                });
+                $scope.boards = boardArr;
+            })
+            .catch((err) => {
+                console.log("error!", err);
+            });
+    }
+    $scope.addPin = (pinObj) => {
+        //takes the pin object and relates it to the parentboard
+    }
+
+    // $scope.formTitle = "Add New Task";
+    $scope.board = {
+        title: "",
+        description: "",
+        addPin: addPin(pinObj),
+        FirebaseId: "",
+        uid: UserFactory.getUser(),
+        uniqueId: `${$scope.board.uid}${$scope.board.FirebaseId}`
+    };
+
+    $scope.saveBoard = () => {
+        BoardFactory.postNewBoard($scope.boardItem)
+            .then((data) => {
+                console.log("new board data", data);
+                $window.location.href = '#!/boards/view';
+            });
+    };
+
+});
--- app/factories/boardFactory.js
+++ app/factories/boardFactory.js
@@ -3,3 +3,40 @@
 // FBCreds info to Here
 // Board Creation form data from user input.
 // post to firebase
+
+app.factory('BoardFactory', function($q, $http, FirebaseUrl, FBCreds) {
+
+    var config = {
+        apiKey: FBCreds.key,
+        authDomain: FBCreds.authDomain
+    };
+
+    firebase.initializeApp(config);
+
+    let getBoardList = (userId) => {
+        console.log("userId", userId);
+        return $q((resolve, reject) => {
+            $http.get(`${FirebaseUrl}pintowin.json?orderBy="uid"&equalTo="${userId}"`)
+                .then((boardData) => {
+                    resolve(boardData);
+                })
+                .catch((err) => {
+                    console.log("oops", err);
+                    reject(err);
+                });
+        });
+    };
+    let postNewBoard = (newBoard) => {
+        return $q((resolve, reject) => {
+            $http.post(`${FirebaseUrl}boards.json`,
+                    angular.toJson(newBoard))
+                .then((newBoardData) => {
+                    resolve(newBoardData);
+                })
+                .catch((err) => {
+                    reject(err);
+                });
+        });
+    };
+
+});
