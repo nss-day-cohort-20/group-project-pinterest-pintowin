e3d08a0dadbb9e95076191e78be6f14743e4e664
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-08-01 11:17:34 -0500

    toggles nav based on login/logout

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -34,11 +34,8 @@ app.controller("UserController", function($scope, $window, UserFactory) {
   };
 
   $scope.register = () => {
-    // TODO validate that user doesn't exist
-    console.log("you clicked register");
     UserFactory.createUser($scope.account)
     .then( (userData) => {
-      console.log("New User!", userData);
       $scope.login();
     });
   };
@@ -46,15 +43,8 @@ app.controller("UserController", function($scope, $window, UserFactory) {
   $scope.login = () => {
     UserFactory.loginUser($scope.account)
     .then( (userData) => {
-      console.log("userData", userData);
       $window.location.href = '#!/todos/view';
     });
   };
-
-  // Moved to nav ctrl
-  // $scope.logout = () => {
-  //   console.log("logout clicked");
-  //   UserFactory.logoutUser();
-  // };
-
+  
 });
